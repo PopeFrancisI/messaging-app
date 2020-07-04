d20080ad8a14d60f64c4c585e2906d572aed3fcb
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-07-04 16:04:49 +0200

    Forgot to add the file in last commit. Now create_db.py is functional.

--- MessagingApp/create_db.py
+++ MessagingApp/create_db.py
@@ -5,6 +5,7 @@ import argparse
 DEFAULT_HOST = 'localhost'
 DEFAULT_USER = 'postgres'
 DEFAULT_PWD = 'coderslab'
+DEFAULT_DBNAME = "messaging_app_db"
 
 
 def create_db():
@@ -16,32 +17,37 @@ def create_db():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-u")
     arg_parser.add_argument("--host")
-    arg_parser.add_argument("--pwd")
+    arg_parser.add_argument("-p")
+    arg_parser.add_argument("--dbname", help="New database name.")
 
     args = arg_parser.parse_args()
 
     user = args.u if args.u else DEFAULT_USER
     host = args.host if args.host else DEFAULT_HOST
-    pwd = args.pwd if args.pwd else DEFAULT_PWD
+    pwd = args.p if args.p else DEFAULT_PWD
+    dbname = args.dbname if args.dbname else DEFAULT_DBNAME
 
-    # create a connection
     try:
+        # create a connection
         connection = connect(user=user, host=host, password=pwd)
         connection.autocommit = True
         cur = connection.cursor()
         # attempt to create a database
-        cur.execute("CREATE DATABASE maly_test_db")
+        sql = f"CREATE DATABASE {dbname}"
+        cur.execute(sql)
     except Exception as e:
-        print('Error', e.pgcode, ":", e)
         cur.close()
         connection.close()
         if e.pgcode == '42P04':
+            print(f'Database {dbname} is already present.')
             return True
         else:
+            print('Error', e.pgcode, ":", e)
             return False
     else:
         cur.close()
         connection.close()
+        print(f'Database {dbname} succesfully created!')
         return True
 
 
