3e036e7284e86606f7ffcbf82abc1251176ef4f2
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-07-05 13:24:15 +0200

    Updated gitignore and implemented some chunks of ma_users.py

--- ma_users.py
+++ ma_users.py
@@ -1,5 +1,18 @@
 from MessagingApp.user import User
+from psycopg2 import connect
+from MessagingApp.database_defaults import DEFAULT_DBNAME, DEFAULT_HOST, DEFAULT_USER, DEFAULT_NEW_DBNAME, DEFAULT_PWD
 import argparse
+
+
+def get_not_none_args(args):
+    args_dict = vars(args)
+    not_none_args_dict = {}
+    for key in args_dict:
+        if args_dict[key]:
+            not_none_args_dict[key] = args_dict[key]
+    return not_none_args_dict
+
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -11,17 +24,61 @@ def parse_args():
     parser.add_argument('-e', '--edit', help="Edit user's password.")
 
     args = parser.parse_args()
-    print(args)
-    return args
+    not_none_args = get_not_none_args(args)
+    return not_none_args
 
 
-def create_user():
+def connect_to_database():
+    try:
+        # create a connection to create the database itself
+        print(f'Connecting to {DEFAULT_NEW_DBNAME}...')
+        connection = connect(user=DEFAULT_USER, host=DEFAULT_HOST, password=DEFAULT_PWD)
+        connection.autocommit = True
+    except Exception as e:
+            print('Error', e.pgcode, ":", e)
+            connection.close()
+            return None
+    return connection
+
+
+def create_user(connection, username, password):
+    user = User.load_user_by_username()
+    if user:
+        print(f"User {username} already exists! Failed to add a new user.")
+        return False
+    else:
+        if len(password) < 8:
+            print(f'Password too short. Failed to add a new user.')
+        else:
+
+            connection.cursor()
+
+
+def edit_user(connection, username, password, new_password):
+    pass
+
+
+def delete_user(connection, username, password):
+    pass
+
+
+def list_all_users(connection):
     pass
 
 
 def process_args(args):
-    if args.username and args.password:
-        return create_user()
+    if args['username'] and args['password'] and len(args) == 2:
+        return create_user(args['username'], args['password'])
+
+    if args['username'] and args['password'] and args['edit'] and args['new_pass'] and len(args) == 4:
+        return edit_user(args['username'], args['password'], args['new_pass'])
+
+    if args['username'] and args['password'] and args['delete'] and len(args) == 3:
+        return delete_user(args['username'], args['password'])
+
+    if args['list'] and len(args) == 1:
+        return list_all_users()
+
 
 
 def main():
