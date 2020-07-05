bed91b813f1c4c947e22dd238cbed6b7b1ccf79f
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-07-05 14:07:30 +0200

    Added working edit_user functionality

--- ma_users.py
+++ ma_users.py
@@ -1,6 +1,7 @@
 from MessagingApp.user import User
 from psycopg2 import connect
 from MessagingApp.database_defaults import DEFAULT_DBNAME, DEFAULT_HOST, DEFAULT_USER, DEFAULT_NEW_DBNAME, DEFAULT_PWD
+from MessagingApp.lib.clcrypto import check_password
 import argparse
 
 
@@ -20,7 +21,7 @@ def parse_args():
     parser.add_argument('-n', '--new_pass', help="New password for user under username.")
     parser.add_argument('-l', '--list', help="List all users.")
     parser.add_argument('-d', '--delete', help="Delete user under username.")
-    parser.add_argument('-e', '--edit', help="Edit user's password.")
+    parser.add_argument('-e', '--edit', help="Edit user's password.", action="store_true")
 
     args = parser.parse_args()
     not_none_args = get_not_none_args(args)
@@ -44,9 +45,11 @@ def create_user(connection, username, password):
     if connection:
         cur = connection.cursor()
         user = User.load_user_by_username(cur, username)
+
         if user:
             print(f"User {username} already exists! Failed to add a new user.")
             return False
+
         if len(password) < 8:
             print(f'Password too short. Failed to add a new user.')
             return False
@@ -55,10 +58,26 @@ def create_user(connection, username, password):
         return new_user.save_to_db(cur)
 
 
-
-
 def edit_user(connection, username, password, new_password):
-    pass
+    if connection:
+        cur = connection.cursor()
+        user = User.load_user_by_username(cur, username)
+
+        if not user:
+            print(f"User {username} does not exist! Failed to update password.")
+            return False
+
+        if not check_password(password, user.hashed_password):
+            print("Wrong password.")
+            return False
+
+        if len(new_password) < 8:
+            print("New password too short. Failed to update password.")
+            return False
+
+        user.set_password(new_password)
+        return user.save_to_db(cur)
+
 
 
 def delete_user(connection, username, password):
@@ -79,7 +98,7 @@ def process_args(args):
 
     if args['username'] and args['password'] and args['edit'] and args['new_pass'] and len(args) == 4:
         if edit_user(connection, args['username'], args['password'], args['new_pass']):
-            print(f"{args['username']} updated successfully.")
+            print(f"{args['username']}'s password updated successfully.")
     #
     # if args['username'] and args['password'] and args['delete'] and len(args) == 3:
     #     return delete_user(connection, args['username'], args['password'])
